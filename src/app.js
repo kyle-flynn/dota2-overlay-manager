040050ba5a350128658d84c286224701250a75c5
Kyle Flynn <kyleflynn.dev@gmail.com>
2021-01-23 02:05:00 -0500

    Updated app.js - works and added states of interest

--- src/app.js
+++ src/app.js
@@ -1,24 +1,23 @@
-var d2gsi = require("dota2-gsi");
-var server = new d2gsi();
-
-var clients = [];
+const d2gsi = require("dota2-gsi");
+const server = new d2gsi({ port: 9001 });
+const selfToken = "killjoy_76561198039309618";
 
 server.events.on("newclient", function (client) {
-  console.log("New client connection, IP address: " + client.ip);
-  if (client.auth && client.auth.token) {
-    console.log("Auth token: " + client.auth.token);
-  } else {
-    console.log("No Auth token");
+  if (client.auth && client.auth.token === selfToken) {
+    console.log("Your token has been detected. You are connected.");
+    client.on("map:game_state", function (state) {
+      if (state === "DOTA_GAMERULES_STATE_HERO_SELECTION") {
+        // TODO - Update OBS overlay - hide team heroes
+      }
+      if (state === "DOTA_GAMERULES_STATE_STRATEGY_TIME") {
+        // TODO - Update OBS overlay - show team heroes
+      }
+      if (state === "DOTA_GAMERULES_STATE_TEAM_SHOWCASE") {
+        // TODO - Update OBS overlay - disable all overlays
+      }
+      if (state === "DOTA_GAMERULES_STATE_PRE_GAME") {
+        // TODO - Update OBS overlay - show wards overlay
+      }
+    });
   }
-  clients.push(client);
 });
-
-setInterval(function () {
-  clients.forEach(function (client, index) {
-    if (client.gamestate.hero && client.gamestate.hero.level) {
-      console.log(
-        "Client " + index + " is level " + client.gamestate.hero.level
-      );
-    }
-  });
-}, 1 * 1000); // Every ten seconds
